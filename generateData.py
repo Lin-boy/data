e5cad32f69ee475d5e577a1f501bf9db3301c436
Lin-boy <43169067+Lin-boy@users.noreply.github.com>
2020-07-24 23:19:48 +0800

    Update generateData.py

--- generateData.py
+++ generateData.py
@@ -1,7 +1,3 @@
-# ----------------------------------------
-# Written by Yude Wang
-# ----------------------------------------
-
 import torch
 import torch.nn as nn
 from datasets.VOCDataset import VOCDataset
